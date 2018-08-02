355bdb81de59ed8f63d0db035902e14c3911e003
mrossabbott <42041368+mrossabbott@users.noreply.github.com>
2018-08-02 10:27:02 -0700

    Update application_vars.sh

--- masterless/application_vars.sh
+++ masterless/application_vars.sh
@@ -1,6 +1,6 @@
-APPLICATION_NAME="mabbotttest"
-DEPLOY_GROUP="mabbotttestgroup-292txed"
-BUCKET="mabbotttest"
+APPLICATION_NAME="mabbotttestdemo"
+DEPLOY_GROUP="mabbotttestdemo-2nsaq0i"
+BUCKET="mabbotttestdemo"
 BUNDLE_PATH="puppet-masterless.tar"
 EC2_TAG_KEY='Puppet'
 EC2_TAG_VALUE='Masterless'
