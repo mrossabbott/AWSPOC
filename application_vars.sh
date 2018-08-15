9b829f2f46082a56eec7faaf8c3eeb4e459ccb28
mrossabbott <42041368+mrossabbott@users.noreply.github.com>
2018-08-15 07:57:23 -0700

    Update and rename application_vars.sh to masterless/application_vars

--- application_vars.sh
+++
@@ -1,6 +0,0 @@
-APPLICATION_NAME="mabbotttestdemo"
-DEPLOY_GROUP="mabbotttestdemo-2nsaq0i"
-BUCKET="mabbotttestdemo"
-BUNDLE_PATH="puppet-masterless.tar"
-EC2_TAG_KEY='Puppet'
-EC2_TAG_VALUE='Masterless'
