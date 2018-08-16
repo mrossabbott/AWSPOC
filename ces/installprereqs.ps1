d51a6fa84fbeecca527c4984e26c19e84a89edfa
mrossabbott <42041368+mrossabbott@users.noreply.github.com>
2018-08-16 11:55:01 -0700

    Update installprereqs.ps1

--- ces/installprereqs.ps1
+++ ces/installprereqs.ps1
@@ -10,5 +10,3 @@
 Start-Process -Wait -Filepath "C:\ces-install\jdk-8u131-windows-x64.exe" -ArgumentList 
 #install tomcat
 Start-Process -Wait -Filepath "C:\ces-install\apache-tomcat-8.0.24.exe" -ArgumentList
-#copy war to deploy location
-Copy-Item "C:\ces-install\concur.war" "C:\apache-tomcat-8.0.24\webapps"
